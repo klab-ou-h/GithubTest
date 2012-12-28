39caeeb402047a3f7eab2157c585e20c1508712b
Alex.Wang <wakin001@gmail.com>
2012-12-28 15:25:53 +0900

    Committed in branch 1.
    Create by branch1

---
+++ GithubTest/TestClassInBranch1.h
@@ -0,0 +1,16 @@
+//
+//  TestClassInBranch1.h
+//  GithubTest
+//
+//  Created by wang feng on 12/12/28.
+//  Copyright 2012年 wang feng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface TestClassInBranch1 : CCNode {
+    
+}
+
+@end
---
+++ GithubTest/TestClassInBranch1.m
@@ -0,0 +1,14 @@
+//
+//  TestClassInBranch1.m
+//  GithubTest
+//
+//  Created by wang feng on 12/12/28.
+//  Copyright 2012年 wang feng. All rights reserved.
+//
+
+#import "TestClassInBranch1.h"
+
+
+@implementation TestClassInBranch1
+
+@end
